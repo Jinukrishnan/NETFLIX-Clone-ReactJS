a1ee61f25fb9ab144939091a51d296ec9f0911d5
jinukrishnan <jinukrishnan.p@gmail.com>
2024-01-17 12:12:47 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { originals,action } from './urls';
+import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div className="App">
+    <NavBar/>
+    <Banner/>
+    <RowPost url={originals} title='Netflix Originals'/>
+    <RowPost url={action} title='Action' isSmall/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Constant/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3'
+export const API_KEY="5badbbabb6d7aeaab586ebdbf7053e3b" 
+export const imageUrl= "https://image.tmdb.org/t/p/original"
